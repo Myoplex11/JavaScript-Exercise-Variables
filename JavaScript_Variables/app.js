abd0174ced22589f1212fe1f49848aae1758ee7b
Santanu Bhattacharya <sxbhatta1@gmail.com>
2022-11-25 18:49:32 -0500

    Add files via upload

---
+++ JavaScript_Variables/app.js
@@ -0,0 +1,42 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+const firstName = "Santanu"
+let lastName;
+var age;
+
+
+
+// Exercise 2
+
+lastName = "Bhattacharya";
+age =  44;
+console.log(`${firstName} ${lastName}`)
+
+
+
+// Exercise 3
+let language = "JavaScript" ;
+let createdYear = 1995;
+let CaseSensitive = true;
+console.log(`${language} was first released in the year ${1995}.`)
+
+// Exercise 4
+let price = 19.99;
+let isOnSale =false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+// Excersise 5
+let title = '"Name of the Wind"';
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark =456;
+let hasRead = true;
+console.log(`The book ${title} was written by ${author}.`)
